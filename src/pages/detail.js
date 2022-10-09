b02a67cea14864afd2bfc5469558d680c03038cc
unknown <mxnzis@gmail.com>
2022-10-09 17:11:12 +0700

    fixing error

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,47 +1,47 @@
-import { Col, Row, Typography, Card } from "antd";
-import React, { useState, useEffect } from "react";
-import data from "../data.json";
-import { useParams } from "react-router-dom";
+// import { Col, Row, Typography, Card } from "antd";
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import data from "../data.json";
 
-const { Title, Text } = Typography;
-const { Meta } = Card;
+// const { Title, Text } = Typography;
+// const { Meta } = Card;
 
 const App = () => {
-  const { key } = useParams();
-  console.log(key);
-  const [detailData, setDetailData] = useState(data);
+  // const { key } = useParams();
+  // console.log(key);
+  // const [detailData, setDetailData] = useState(data);
 
-  useEffect(() => {
-    const filterData = data.filter((e) => e.recipe_id == key);
-    setDetailData(filterData);
-  }, [key]);
-  console.log("name", detailData[0].name);
-  return (
-    <>
-      <Row>
-        <Col span={8}>
-          <Card
-            hoverable
-            style={{ width: 300 }}
-            cover={
-              <img
-                alt="example"
-                height={240}
-                src={detailData[0].image}
-              />
-            }
-          >
-            <Meta title={`${detailData[0].liked} Likes` } />
-          </Card>
-        </Col>
-        <Col span={16}>
-          <Title level={2}>{detailData[0].name}</Title>
-          <Title level={5}>{detailData[0].category}</Title>
-          <Text>{detailData[0].ingredients}</Text>
-        </Col>
-      </Row>
-    </>
-  );
+  // useEffect(() => {
+  //   const filterData = data.filter((e) => e.recipe_id == key);
+  //   setDetailData(filterData);
+  // }, [key]);
+  // console.log("name", detailData[0].name);
+  // return (
+  //   <>
+  //     <Row>
+  //       <Col span={8}>
+  //         <Card
+  //           hoverable
+  //           style={{ width: 300 }}
+  //           cover={
+  //             <img
+  //               alt="example"
+  //               height={240}
+  //               src={detailData[0].image}
+  //             />
+  //           }
+  //         >
+  //           <Meta title={`${detailData[0].liked} Likes` } />
+  //         </Card>
+  //       </Col>
+  //       <Col span={16}>
+  //         <Title level={2}>{detailData[0].name}</Title>
+  //         <Title level={5}>{detailData[0].category}</Title>
+  //         <Text>{detailData[0].ingredients}</Text>
+  //       </Col>
+  //     </Row>
+  //   </>
+  // );
 };
 
 export default App;
